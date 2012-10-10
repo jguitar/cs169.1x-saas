6392e028971b6419dce85ffbaceaa26dd093afcc
Juan F. Pérez <j.guitar@gmail.com>
2012-10-10 23:18:22 +0200

    hw 1 part 4

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -1,23 +1,80 @@
 class Dessert
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
-    # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
-  
+
   def healthy?
-    # YOUR CODE HERE
+    calories < 200
   end
-  
+
   def delicious?
-    # YOUR CODE HERE
+    true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
+
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    super(name, calories)
+    @flavor = flavor
   end
-  
+
   def delicious?
-    # YOUR CODE HERE
+    !(@flavor == 'black licorice')
   end
 end
+
+
+# test
+dessert1 = Dessert.new('cake', 100)
+if dessert1.healthy?
+  puts 'ok'
+else
+  puts 'error'
+end
+if dessert1.delicious?
+  puts 'ok'
+else
+  puts 'error'
+end
+
+dessert2 = Dessert.new('sugar', 300)
+unless dessert2.healthy?
+  puts 'ok'
+else
+  puts 'error'
+end
+if dessert2.delicious?
+  puts 'ok'
+else
+  puts 'error'
+end
+
+jelly1 = JellyBean.new('tinky', 100, 'brown')
+if jelly1.healthy?
+  puts 'ok'
+else
+  puts 'error'
+end
+if jelly1.delicious?
+  puts 'ok'
+else
+  puts 'error'
+end
+
+jelly2 = JellyBean.new('tinky', 300, 'black licorice')
+unless jelly2.healthy?
+  puts 'ok'
+else
+  puts 'error'
+end
+unless jelly2.delicious?
+  puts 'ok'
+else
+  puts 'error'
+end
+
