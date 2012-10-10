6a08e16317668a49f6167f66813b31b88613fe19
Juan F. Pérez <j.guitar@gmail.com>
2012-10-10 20:50:29 +0200

    adding code for rps_tournament_winner and fixing samples

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -2,7 +2,10 @@ class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
 def rps_result(m1, m2)
-  # YOUR CODE HERE
+  (m1.upcase == m2.upcase) or
+    ((m1.upcase == 'R') and (m2.upcase == 'S')) or
+    ((m1.upcase == 'S') and (m2.upcase == 'P')) or
+    ((m1.upcase == 'P') and (m2.upcase == 'R'))
 end
 
 def rps_game_winner(game)
@@ -17,18 +20,24 @@ def rps_game_winner(game)
   name1, move1 = player1
   name2, move2 = player2
 
-  # The rules are: R beats S; S beats P; and P beats R
-  return name1 if (move1.upcase == move2.upcase)
-  return name1 if (move1.upcase == 'R') and (move2.upcase == 'S')
-  return name1 if (move1.upcase == 'S') and (move2.upcase == 'P')
-  return name1 if (move1.upcase == 'P') and (move2.upcase == 'R')
-  return name2
+  rps_result(move1, move2) ? player1 : player2
 end
 
 def rps_tournament_winner(tournament)
-  # YOUR CODE HERE
-end
+  roundA, roundB = tournament
+  if roundA[0].class == String
+    player1 = roundA
+    player2 = roundB
+  else
+    player1 = rps_tournament_winner(roundA)
+    player2 = rps_tournament_winner(roundB)
+  end
 
+  game = []
+  game << player1
+  game << player2
+  return rps_game_winner(game)
+end
 
 # test
 begin
@@ -46,31 +55,43 @@ rescue NoSuchStrategyError
   puts 'ok'
 end
 
-if rps_game_winner([['Test1', 'P'], ['Test2', 'P']]) == 'Test1'
+if rps_game_winner([['Test1', 'P'], ['Test2', 'P']]) == ['Test1', 'P']
   puts 'ok'
 else
   puts 'error'
 end
 
-if rps_game_winner([['Test1', 'R'], ['Test2', 'S']]) == 'Test1'
+if rps_game_winner([['Test1', 'R'], ['Test2', 'S']]) == ['Test1', 'R']
   puts 'ok'
 else
   puts 'error'
 end
 
-if rps_game_winner([['Test1', 'S'], ['Test2', 'P']]) == 'Test1'
+if rps_game_winner([['Test1', 'S'], ['Test2', 'P']]) == ['Test1', 'S']
   puts 'ok'
 else
   puts 'error'
 end
 
-if rps_game_winner([['Test1', 'P'], ['Test2', 'R']]) == 'Test1'
+if rps_game_winner([['Test1', 'P'], ['Test2', 'R']]) == ['Test1', 'P']
   puts 'ok'
 else
   puts 'error'
 end
 
-if rps_game_winner([['Test1', 'S'], ['Test2', 'R']]) == 'Test2'
+if rps_game_winner([['Test1', 'S'], ['Test2', 'R']]) == ['Test2', 'R']
+  puts 'ok'
+else
+  puts 'error'
+end
+
+if rps_tournament_winner([[[["Armando", "P"], ["Dave", "S"]], [["Richard", "R"],  ["Michael", "S"]]], [[["Allen", "S"], ["Omer", "P"]], [["David E.", "R"], ["Richard X.", "P"]]]]) == ["Richard", "R"]
+  puts 'ok'
+else
+  puts 'error'
+end
+
+if rps_tournament_winner([[[["Armando", "R"], ["Dave", "R"]], [["Richard", "S"],  ["Michael", "P"]]], [[["Allen", "S"], ["Omer", "R"]], [["David E.", "P"], ["Richard X.", "S"]]]]) == ["Armando", "R"]
   puts 'ok'
 else
   puts 'error'
