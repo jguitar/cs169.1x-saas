68d2ed2c6ed426b83c77b97163fe2694ddef7d8c
Juan F. Pérez <j.guitar@gmail.com>
2012-10-14 19:01:20 +0200

    hw1/part7 and tests

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -1,4 +1,41 @@
 class CartesianProduct
   include Enumerable
-  # YOUR CODE HERE
+
+  def initialize(collection1, collection2)
+    @collection1 = collection1
+    @collection2 = collection2
+  end
+
+  def each
+    @collection1.each do |valueA|
+      @collection2.each do |valueB|
+        yield([valueA, valueB])
+      end
+    end
+  end
 end
+
+
+# test
+puts 'Sample1'
+c = CartesianProduct.new([:a, :b], [4, 5])
+c.each { |elt| puts elt.inspect }
+# [:a, 4]
+# [:a, 5]
+# [:b, 4]
+# [:b, 5]
+
+puts ''
+puts 'Sample2'
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+# Nothing printed since Cartesian product of anything with an empty collection is empty
+
+puts ''
+puts 'Sample3'
+c = CartesianProduct.new([:a, :b, :c], [4, 5])
+c.each { |elt| puts elt.inspect }
+# [:a, 4]
+# [:a, 5]
+# [:b, 4]
+# [:b, 5]
